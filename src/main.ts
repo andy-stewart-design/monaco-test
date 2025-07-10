738f556f7fa3498ecbcc40b6444b3d446ae25c6e
andy-stewart-design <robestewart@ebay.com>
2025-07-10 15:34:58 -0400

    removing unnecessary worker code

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,13 @@
 // https://framagit.org/roipoussiere/strudel-vscode <- sturdel vscode extension with syntax highlighting
 // https://www.checklyhq.com/blog/customizing-monaco/ <- adding custom languages to monaco
 
-import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
-import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 import * as monaco from "monaco-editor";
 import "./style.css";
 
 self.MonacoEnvironment = {
-  getWorker(_, label) {
-    if (label === "json") {
-      return new jsonWorker();
-    }
-    if (label === "css" || label === "scss" || label === "less") {
-      return new cssWorker();
-    }
-    if (label === "html" || label === "handlebars" || label === "razor") {
-      return new htmlWorker();
-    }
-    if (label === "typescript" || label === "javascript") {
-      return new tsWorker();
-    }
-    return new editorWorker();
+  getWorker() {
+    return new tsWorker();
   },
 };
 
